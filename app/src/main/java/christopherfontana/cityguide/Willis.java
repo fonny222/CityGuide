da35ca0436abd2e5140a0540460ee0bff485f04b
fonny222 <christopherdfontana@gmail.com>
2017-02-15 22:56:44 -0600

    city guide layout app
    multi view layout for a navigation type application using web uri's and
    pictures in different classes

---
+++ app/src/main/java/christopherfontana/cityguide/Willis.java
@@ -0,0 +1,14 @@
+package christopherfontana.cityguide;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Willis extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_willis);
+    }
+
+}
